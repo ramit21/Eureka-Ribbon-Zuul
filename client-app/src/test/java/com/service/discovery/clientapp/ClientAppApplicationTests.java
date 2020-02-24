db489d0867163a844293d528c440bd6f4b3dea9b
21.ramit@gmail.com <21.ramit@gmail.com>
2020-02-25 01:50:29 +0530

    Initial commit

---
+++ client-app/src/test/java/com/service/discovery/clientapp/ClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.discovery.clientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
